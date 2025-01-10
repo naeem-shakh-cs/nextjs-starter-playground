f4f984caa76cdcf280a6f900e58975bf385abc8a
Naeem Shaikh <naeem.shaikh@contentstack.com>
2025-01-10 15:48:18 +0530

    p

--- next.config.js
+++ next.config.js
@@ -1,18 +1,27 @@
-/** @type {import('next').NextConfig} */
+// next.config.js
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'max-age=0, s-maxage=86400, stale-while-revalidate',
+    async headers() {
+      return [
+        {
+          source: "/",
+          headers: [
+            {
+                key: "cache-control",
+                value: "max-age=0, s-maxage=86400, stale-while-revalidate",
+          // The browser should validate the response with CDN, but the CDN can serve cached responses for 86400 seconds(24 hours), after which stale response will be returned while also triggering a revalidation in the background.
+            },
+          ],
+        },{
+            source: "/hi",
+            headers: [
+              {
+                  key: "cache-control",
+                  value: "max-age=0, s-maxage=600, stale-while-revalidate",
+            // The browser should validate the response with CDN, but the CDN can serve cached responses for 86400 seconds(24 hours), after which stale response will be returned while also triggering a revalidation in the background.
+              },
+            ],
           },
-        ],
-      },
-    ];
-  }
-}
-
-module.exports = nextConfig;
+      ];
+    },
+  };
+  module.exports = nextConfig;
