ea6d4f7ed2aa91b1a29e8072d2744d4603ad67d7
naeem-shakh-cs <165668796+naeem-shakh-cs@users.noreply.github.com>
2025-01-10 14:10:52 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async headers() {
+        return [
+            {
+                source: "/",
+                headers: [
+                    {
+                        key: "Cache-Control",
+                        value: "public, max-age=0, s-maxage=600, stale-while-revalidate=10"
+                    }
+                ]
+            }
+        ];
+    }
+}
 
 module.exports = nextConfig
