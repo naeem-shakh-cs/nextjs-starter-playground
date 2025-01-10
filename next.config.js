68a31d5304aa5ffd3df6bd7fcebbaf2e53c7b03f
Naeem Shaikh <naeem.shaikh@contentstack.com>
2025-01-10 15:31:19 +0530

    t

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async headers() {
-        return [
-            {
-                source: "/(.*)",
-                headers: [
-                    {
-                        key: "cache-control",
-                        value: "max-age=0, s-maxage=86400, stale-while-revalidate"
-                    }
-                ]
-            }
-        ];
-    }
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'max-age=0, s-maxage=86400, stale-while-revalidate',
+          },
+        ],
+      },
+    ];
+  }
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
