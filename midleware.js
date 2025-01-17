e7062cb16481195d3eb78dcd504221c888f4b49e
Naeem Shaikh <naeem.shaikh@contentstack.com>
2025-01-17 19:45:35 +0530

    cache control on hi with rsc

--- midleware.js
+++ midleware.js
@@ -8,6 +8,12 @@ export function middleware(req) {
     response.headers.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
     return response;
   }
+  if (url.pathname === 'hi' && url.searchParams.has('rsc')) {
+    const response = NextResponse.next();
+    // Set 'Cache-Control' header to prevent caching
+    response.headers.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+    return response;
+  }
   // Continue processing for other requests
   return NextResponse.next();
 }
