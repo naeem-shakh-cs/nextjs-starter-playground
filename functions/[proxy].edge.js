8516b5ba2e3cbaba340b888e2f200a790c1aae0e
Naeem Shaikh <naeem.shaikh@contentstack.com>
2025-01-17 22:30:01 +0530

    log

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -3,6 +3,13 @@ export default function handler(request, context) {
     const route = parsedUrl.pathname;
     const rscQueryParamExists = !!parsedUrl.searchParams.get('rsc')
     const rscHeaderExists = request.headers.get('rsc')  ==='1'
+    if(route === '/hi'){
+        // log all headers
+        console.log('---headers or hi')
+        for (let i=0; i<request.headers.length; i++) {
+            console.log(request.headers[i]);
+        }
+    }
     console.log(request.url, rscQueryParamExists, rscHeaderExists)
     if (route === '/hi' && rscQueryParamExists && !rscHeaderExists) {
         const modifiedRequest = new Request(request);
