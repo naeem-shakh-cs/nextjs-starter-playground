d2235171072b507f499e23af32df952963680055
Naeem Shaikh <naeem.shaikh@contentstack.com>
2025-01-17 22:39:33 +0530

    format

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -2,7 +2,7 @@ export default function handler(request, context) {
     const parsedUrl = new URL(request.url);
     const route = parsedUrl.pathname;
     const rscQueryParamExists = !!parsedUrl.searchParams.get('rsc')
-    const rscHeaderExists = request.headers.get('rsc')  ==='1'
+    const rscHeaderExists = request.headers.get('rsc')  === '1'
 
     console.log(request.url, rscQueryParamExists, rscHeaderExists)
     if (route === '/hi' && !rscQueryParamExists && rscHeaderExists) {
