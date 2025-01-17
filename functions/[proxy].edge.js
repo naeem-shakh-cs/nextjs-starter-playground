c673fe39567ae177922d0f7cc030eb722a19cc97
Naeem Shaikh <naeem.shaikh@contentstack.com>
2025-01-17 22:45:03 +0530

    list

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -1,15 +1,18 @@
+const RSC_REDIRECT_PATHS = [
+    '/hi'
+];
+
 export default function handler(request, context) {
     const parsedUrl = new URL(request.url);
     const route = parsedUrl.pathname;
-    const rscQueryParamExists = !!parsedUrl.searchParams.get('rsc')
-    const rscHeaderExists = request.headers.get('rsc')  === '1'
+    const rscQueryParamExists = !!parsedUrl.searchParams.get('rsc');
+    const rscHeaderExists = request.headers.get('rsc') === '1';
+    const isRscRedirectPath = RSC_REDIRECT_PATHS.indexOf(route) > -1;
 
-    console.log(request.url, rscQueryParamExists, rscHeaderExists)
-    if (route === '/hi' && !rscQueryParamExists && rscHeaderExists) {
+    if (isRscRedirectPath && !rscQueryParamExists && rscHeaderExists) {
         const modifiedRequest = new Request(request);
         modifiedRequest.headers.delete('rsc');
         return fetch(modifiedRequest);
     }
-    return fetch(request)
+    return fetch(request);
 }
-   
